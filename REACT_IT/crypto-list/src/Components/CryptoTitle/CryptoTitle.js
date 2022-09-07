163a791a5635b17551ef4990238211d0d8cdca6c
aldinhalilovic <aldinhalilovic06@gmail.com>
2022-09-07 23:54:45 +0200

    bulma test

--- REACT_IT/crypto-list/src/Components/CryptoTitle/CryptoTitle.js
+++ REACT_IT/crypto-list/src/Components/CryptoTitle/CryptoTitle.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./CryptoTitle.css";
+import "bulma/css/bulma.css";
 
 const CryptoTitle = () => {
   return (
     <div className="crypto-title">
       <h1>INSERT CRYPTO DATA</h1>
+      <button class="button is-white">White</button>
+      <button class="button is-black">Black</button>
+      <button
+        class="button is-primary is-large"
+        title="Disabled button"
+        disabled
+      >
+        Disabled
+      </button>
     </div>
   );
 };
